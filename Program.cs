5dde1cd52978b3169d75e246df3a2f0ec833c087
dezlez <alexdesellier@gmail.com>
2016-04-06 18:56:25 -0400

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 22 / 7m;
+            Console.WriteLine(pi);
+            Console.ReadLine();
+
+        }
+
+    }
+}
